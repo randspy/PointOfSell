9c6dbf873695db813ecab353e52b0065432e8bff
sgluter <mos.koziel@gmail.com>
2015-07-25 12:27:34 +0200

    Refactoring: Object creation moved to setup method

--- test/source/com/randspy/test/point_of_sell/ShowPriceOnDisplay.java
+++ test/source/com/randspy/test/point_of_sell/ShowPriceOnDisplay.java
@@ -1,29 +1,33 @@
 package com.randspy.test.point_of_sell;
 import com.randspy.point_of_sell.PointOfSell;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ShowPriceOnDisplay {
 
+    private PointOfSell pointOfSell;
+
+    @Before
+    public void setUp() throws Exception {
+        pointOfSell = new PointOfSell();
+    }
+
     @Test
     public void noInput() throws Exception {
-        PointOfSell pointOfSell = new PointOfSell();
         pointOfSell.onBarcode("");
         assertEquals("No input code", pointOfSell.displayLastText());
     }
 
     @Test
     public void invalidInput() throws Exception {
-        PointOfSell pointOfSell = new PointOfSell();
         pointOfSell.onBarcode("xyz");
         assertEquals("Invalid code", pointOfSell.displayLastText());
     }
 
-
     @Test
     public void whenOnBarcodeNeverCalledDisplayShouldStayEmpty() throws Exception {
-        PointOfSell pointOfSell = new PointOfSell();
         assertEquals("", pointOfSell.displayLastText());
     }
 }
