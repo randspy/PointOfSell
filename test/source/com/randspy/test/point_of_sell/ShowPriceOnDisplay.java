493e59a52acc83ca4849bd26a583ce182487dff4
sgluter <mos.koziel@gmail.com>
2015-07-25 11:44:16 +0200

    Project's setup.

---
+++ test/source/com/randspy/test/point_of_sell/ShowPriceOnDisplay.java
@@ -0,0 +1,12 @@
+package com.randspy.test.point_of_sell;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShowPriceOnDisplay {
+
+    @Test
+    public void nothing() throws Exception {
+        assertEquals(1, 1);
+    }
+}
